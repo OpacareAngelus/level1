e855865fb77267bb8ae8e599444aaa1ec1f12b4d
OpacareAngelus <42981839+OpacareAngelus@users.noreply.github.com>
2022-09-19 16:40:55 +0300

    Update MyProfile.kt

--- app/src/main/java/view/MyProfile.kt
+++ app/src/main/java/view/MyProfile.kt
@@ -1,12 +1,6 @@
 package com.example.myapplication2.view
 
-import android.R.attr.bitmap
-import android.graphics.BitmapShader
-import android.graphics.Paint
-import android.graphics.RectF
-import android.graphics.Shader
 import android.os.Bundle
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication2.R
@@ -23,7 +17,7 @@ class MyProfile : AppCompatActivity() {
         setContentView(R.layout.my_profile)
 
         //In this part program take info from auth_activity. It's firstname and second name.
-        name = findViewById(R.id.name)
+        name = findViewById(R.id.tv_name)
         //Program set this firstname and second name in user profile.
         name.text = intent.getStringExtra("name")
     }
