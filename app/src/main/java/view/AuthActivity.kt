2bd3b98824b1625e64081037c7b5d9aced3e83fa
OpacareAngelus <42981839+OpacareAngelus@users.noreply.github.com>
2022-09-19 16:40:52 +0300

    Update AuthActivity.kt

--- app/src/main/java/view/AuthActivity.kt
+++ app/src/main/java/view/AuthActivity.kt
@@ -1,4 +1,4 @@
-package com.example.myapplication2.view
+package view
 
 import android.content.Intent
 import android.content.SharedPreferences
@@ -11,6 +11,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.myapplication2.R
 import com.example.myapplication2.util.Parsers
+import com.example.myapplication2.view.MyProfile
 import com.google.android.material.snackbar.Snackbar
 
 class AuthActivity : AppCompatActivity(), View.OnClickListener {
@@ -30,20 +31,20 @@ class AuthActivity : AppCompatActivity(), View.OnClickListener {
 
         setContentView(R.layout.activity_auth)
         //Program search all buttons and set listener.
-        btnReg = findViewById(R.id.reg_button)
+        btnReg = findViewById(R.id.btn_registration)
         btnReg.setOnClickListener(this)
-        btnSignIn = findViewById(R.id.sign_in_button)
+        btnSignIn = findViewById(R.id.btn_signIn)
         btnSignIn.setOnClickListener(this)
-        rememberBtn = findViewById(R.id.rememberBtn)
+        rememberBtn = findViewById(R.id.btn_remember)
 
         //Program create file with user data as shared preference.
         users = getSharedPreferences("Users", MODE_PRIVATE)
 
         //Program search all elements which need and add to vars.
         mainView = findViewById(R.id.auth_activity)
-        emailField = findViewById(R.id.e_mail_edit_text)
-        passField = findViewById(R.id.password_edit_text)
-        btnSignIn = findViewById(R.id.sign_in_button)
+        emailField = findViewById(R.id.et_email)
+        passField = findViewById(R.id.et_password)
+        btnSignIn = findViewById(R.id.btn_signIn)
 
         loadText()
     }
@@ -51,10 +52,10 @@ class AuthActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(v: View) {
         val intent = Intent(this, MyProfile::class.java)
         when (v.id) {
-            R.id.reg_button -> {
+            R.id.btn_registration -> {
                 regUser(intent)
             }
-            R.id.sign_in_button -> {
+            R.id.btn_signIn -> {
                 checkPass(intent)
             }
             else -> {}
@@ -69,14 +70,14 @@ class AuthActivity : AppCompatActivity(), View.OnClickListener {
     private fun checkPass(intent: Intent) {
         if (!allChecks())
             return
-        var userName = Parsers().parseMail(emailField.text.toString())
+        val userName = Parsers().parseMail(emailField.text.toString())
         val userData = users.getString(userName, "")?.split("&")?.last()
         if (userData.equals(passField.text.toString())) {
             intent.putExtra("name", userName)
             startActivity(intent)
             overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
         } else {
-            Snackbar.make(mainView, "Incorrect password.", Snackbar.LENGTH_SHORT).show()
+            passField.error = ("Incorrect password.")
             return
         }
     }
@@ -86,15 +87,11 @@ class AuthActivity : AppCompatActivity(), View.OnClickListener {
      * Next time user need put "Sigh in" when user use correct e-mail and password. */
     private fun regUser(intent: Intent) {
         if (emailCheck() && passwordCheck()) {
-            var userName = Parsers().parseMail(emailField.text.toString())
-            var ed: SharedPreferences.Editor = users.edit()
+            val userName = Parsers().parseMail(emailField.text.toString())
+            val ed: SharedPreferences.Editor = users.edit()
             val validTest = users.getString(userName, "")?.split("&")?.first().toString()
             if (validTest == emailField.text.toString()) {
-                Snackbar.make(
-                    mainView,
-                    "User with this e-mail is now registered.",
-                    Snackbar.LENGTH_SHORT
-                ).show()
+                emailField.error = ("User with this e-mail is now registered.")
                 return
             } else {
                 ed.putString(userName, "${emailField.text}&${passField.text}")
@@ -112,17 +109,18 @@ class AuthActivity : AppCompatActivity(), View.OnClickListener {
     private fun allChecks(): Boolean {
         //When e-mail didn't used before and sure password wrong too.
         if (!emailCheck() && !passwordCheck()) {
-            Snackbar.make(mainView, "Invalid email and password.", Snackbar.LENGTH_SHORT).show()
+            emailField.error = ("Invalid email.")
+            passField.error = ("Invalid password.")
             return false
         }
         //When e-mail didn't used before.
         if (!emailCheck() && passwordCheck()) {
-            Snackbar.make(mainView, "Invalid email.", Snackbar.LENGTH_SHORT).show()
+            emailField.error = ("Invalid email.")
             return false
         }
         //When password is wrong for this e-mail.
         if (emailCheck() && !passwordCheck()) {
-            Snackbar.make(mainView, "Invalid password.", Snackbar.LENGTH_SHORT).show()
+            passField.error = ("Invalid password.")
             return false
         }
         return true
@@ -152,11 +150,12 @@ class AuthActivity : AppCompatActivity(), View.OnClickListener {
     /**Fun saving current text in the fields of e-mail and password in the shared pref.*/
     private fun saveText() {
         sPref = getPreferences(MODE_PRIVATE)
-        var ed: SharedPreferences.Editor = sPref.edit()
+        val ed: SharedPreferences.Editor = sPref.edit()
         ed.putString("e_mail", emailField.text.toString())
         ed.putString("password", passField.text.toString())
         ed.apply()
     }
+
     /**Fun loading  text to the fields of e-mail and password from the shared pref.*/
     private fun loadText() {
         sPref = getPreferences(MODE_PRIVATE)
