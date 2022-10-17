82043ea16ce433df94d9c0ea1e569cd23bbe2849
OpacareAngelus <42981839+OpacareAngelus@users.noreply.github.com>
2022-10-17 19:58:30 +0300

    Custome google btn refactoring

---
+++ app/src/main/java/extension/Context.kt
@@ -0,0 +1,12 @@
+package extension
+
+import android.content.Context
+
+
+
+    fun Context.dpToPx(dp: Int): Float {
+        return dp.toFloat() * this.resources.displayMetrics.density
+    }
+
+
+
---
+++ app/src/main/java/extension/Int.kt
@@ -0,0 +1,24 @@
+package extension
+
+import android.content.Context
+import android.util.TypedValue
+
+
+
+/*fun Float.convertDpToPixels( context: Context): Float{
+        return TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            this.toFloat(),
+            context.resources.displayMetrics
+        )
+
+    }*/
+
+    fun Int.convertSpToPixels(context: Context): Int {
+        return TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_SP,
+            this.toFloat(),
+            context.resources.displayMetrics
+        )
+            .toInt()
+    }
